3d861e2f620d20dae2f3eda5e25304cf748935bf
LunarTear-22 <167991393+LunarTear-22@users.noreply.github.com>
2024-05-03 17:24:53 +0200

    Aggiunta insert aggiornate

---
+++ db/Insert.sql
@@ -0,0 +1,29 @@
+USE SwitchAndKeys;
+
+INSERT INTO Catalogo VALUES("1", "12", "Primo catalogo e-commerce Switch&Keys");
+
+INSERT INTO Prodotto VALUES("1","1","MOD 007 V3 HE Year of Dragon - Akko","192.80","22.00","6","Tastiera","75%","","Akko","1","0","0","Tastiera meccanica 75% (Akko) con profilo keycaps Cherry e switch Akko Cream Yellow Magnetic","1");
+INSERT INTO Prodotto VALUES("2","1","MonsGeek M1 - Akko","154.70","22.00","3","Tastiera","75%","","Akko","1","0","0","Tastiera meccanica 75% (Akko) con profilo keycaps OEM e switch Akko V3 Cream Blue Pro","1");
+INSERT INTO Prodotto VALUES("3","1","Blue on White 3068B Plus ISO - Akko","75","22.00","8","Tastiera","65%","","Akko","1","1","1","Tastiera meccanica 65% (Akko) con profilo keycaps Cherry e switch Akko CS Jelly Pinkl","1");
+INSERT INTO Prodotto VALUES("4","1","Racoon MAO Keycap Set (142-Key)","54.70","22.00","0","Keycaps","MAO","PBT","Akko",NULL,NULL,NULL,"Set di 142 keycaps in PBT Double Shot, Profilo MAO (Akko)","0");
+INSERT INTO Prodotto VALUES("5","1","DROP + GMK RED SAMURAI KEYCAP SET (Kit 65%)","75","22.00","10","Keycaps","Cherry","PBT Double Shot","DROP",NULL,NULL,NULL,"Set di keycaps per tastiere in formato 65%, Materiale: PBT Double Shot, Profilo Cherry (DROP x GMK Keycaps)","1");
+INSERT INTO Prodotto VALUES("6","1","EPOMAKER Space Travel Keycaps","36","22.00","2","Keycaps","MDA","PBT","EPOMAKER",NULL,NULL,NULL,"Set di 147 keycaps in PBT, Profilo: MDA (EPOMAKER) ","1");
+INSERT INTO Prodotto VALUES("7","1","Holy Panda Switch (36 pz.) - Lubed","41.90","22.00","20","Switch","Tattili",NULL,"DROP",NULL,NULL,NULL,"Set di 36 Switch Tattili pre-lubrificati","1");
+INSERT INTO Prodotto VALUES("8","1","Wuque WS Jade Linear Switches","0.40","22.00","70","Switch","Lineare",NULL,"Wuque Studio",NULL,NULL,NULL,"Switch(singolo) lineare","1");
+INSERT INTO Prodotto VALUES("9","1","Epomaker Budgerigar Switch Set (35 pz.)","12.99","22.00","25","Switch","Tattili",NULL,"EPOMAKER",NULL,NULL,NULL,"Set di 36 Switch Tattili","1");
+INSERT INTO Prodotto VALUES("10","1","Epomaker MIX Cable V2","19.99","22.00","15","Accessori","Cavo",NULL,"EPOMAKER",NULL,NULL,NULL,"Cavo a spirale da USB C a USB A a doppia manica per tastiere meccaniche","1");
+INSERT INTO Prodotto VALUES("11","1","MXRSKEY DM Stabilizer","9.99","22.00","0","Accessori","Stabilizzatori",NULL,"EPOMAKER",NULL,NULL,NULL,"Set di stabilizzatori montati su piastra POM","0");
+INSERT INTO Prodotto VALUES("12","1","Zoom75 - Addons Extra Batch 1","69","22.00","9","Accessori","PCB",NULL,"Meletrix","1","1","1","Bluetooth/VIA Wired e Wireless Hotswap PCB - Flex cut o non flex CUT","1");
+
+INSERT INTO Immagine VALUES("1","MOD 007 V3 HE Year of Dragon - Akko.png",NULL,"1");
+INSERT INTO Immagine VALUES("2","MonsGeek M1 - Akko.png",NULL,"2");
+INSERT INTO Immagine VALUES("3","Blue on White 3068B Plus ISO - Akko.jpg",NULL,"3");
+INSERT INTO Immagine VALUES("4","Racoon MAO Keycap Set (142-Key).png",NULL,"4");
+INSERT INTO Immagine VALUES("5","DROP + GMK RED SAMURAI KEYCAP SET (Kit 65%).jpg",NULL,"5");
+INSERT INTO Immagine VALUES("6","EPOMAKER Space Travel Keycaps.png",NULL,"6");
+INSERT INTO Immagine VALUES("7","Holy Panda Switch (36 pz.) - Lubed.png",NULL,"7");
+INSERT INTO Immagine VALUES("8","Wuque WS Jade Linear Switches.png",NULL,"8");
+INSERT INTO Immagine VALUES("9","Epomaker Budgerigar Switch Set (35 pz.).png",NULL,"9");
+INSERT INTO Immagine VALUES("10","Epomaker MIX Cable V2.png",NULL,"10");
+INSERT INTO Immagine VALUES("11","MXRSKEY DM Stabilizer.png",NULL,"11");
+INSERT INTO Immagine VALUES("12","Zoom75 - Addons Extra Batch 1.png",NULL,"12");
